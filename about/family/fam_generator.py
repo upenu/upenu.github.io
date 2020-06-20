82ae0064a464dd746bf465b72587808980bf9631
Leon Ming <leon.ming@berkeley.edu>
2020-06-20 15:35:07 -0700

    edited some copy and formatting

--- about/family/fam_generator.py
+++ about/family/fam_generator.py
@@ -1,5 +1,7 @@
 import json
 import random
+import sys
+from collections import deque
 
 tree = { "name": "Nu Chapter", "year": "Founded in 1995", "children": [] }
 ppl = { "Nu Chapter, Founded in 1995": tree}
@@ -54,6 +56,19 @@ with open("famtree.txt", "r") as data:
         littles = fam[1:]
         for little in littles:
             connect(big, little)
+
+fams = tree["children"]
+fams.sort(key=lambda x: -len(repr(x)))
+new_fams = deque()
+for i in range(0, len(tree["children"]), 2):
+    new_fams.appendleft(fams[i])
+    if i + 1 < len(fams):
+        new_fams.append(fams[i + 1])
+assert(len(fams) == len(new_fams))
+tree["children"] = list(new_fams)
+for fam in tree["children"]:
+    [print("1", end="") for x in range(len(repr(fam))//10)]
+    print()
 
 with open("famtree.json", "w") as output:
     json.dump(tree, output)
